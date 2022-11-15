e57955201faf983798d4098a75905d04083e3b5b
G Haridev <haridevgireesh@gmail.com>
2022-11-16 01:20:12 +0530

    Initial commit.

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page, name='secret'),
+    path('secret2/', views.SecretPage.as_view(), name='secret2'),
+]
