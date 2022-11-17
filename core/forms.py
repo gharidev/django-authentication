a8a4a0b9c2e2d1b7a9c9f6d6c63ced4ac0a5092b
G Haridev <haridevgireesh@gmail.com>
2022-11-17 09:19:55 +0530

    Changes in views and models

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import forms
 
-from core.models import User
+# from core.models import User
+# from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(forms.UserCreationForm):
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["email", "username", "age"]
         field_classes = {"username": forms.UsernameField}
 
--- django_authentication/urls.py
+++ django_authentication/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.views import LoginView
+
 urlpatterns = [
     path("", include("core.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path('admin/', admin.site.urls),
 ]
